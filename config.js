af96599c206d8553d9517ed305eb66a5f9999076
RajmondKurti112 <159893027+RajmondKurti112@users.noreply.github.com>
2024-02-16 11:24:39 +0000

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,18 @@ const config = {
   },
 }
 
-config.users[process.env.USER1] = process.env.USER1P;
-config.users[process.env.USER2] = process.env.USER2P;
-config.users[process.env.USER3] = process.env.USER3P;
+// Assigning properties dynamically if environment variables are defined
+if (process.env.USER1 && process.env.USER1P) {
+  config.users[process.env.USER1] = process.env.USER1P;
+}
+if (process.env.USER2 && process.env.USER2P) {
+  config.users[process.env.USER2] = process.env.USER2P;
+}
+if (process.env.USER3 && process.env.USER3P) {
+  config.users[process.env.USER3] = process.env.USER3P;
+}
+
+// Adding a hardcoded user
 config.users.interstellar = 'password';
 
 export default config;
