44e50174bb2b3d6106253976809a2f672799aceb
RajmondKurti112 <159893027+RajmondKurti112@users.noreply.github.com>
2024-02-16 13:39:32 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
 const config = {
-  routes: true, // Change this to false if you just want to host a bare server.
-  local: true, // Change this to false to disable local assets.
+  routes: false, // Change this to false if you just want to host a bare server.
+  local: false, // Change this to false to disable local assets.
   challenge: true,
   users: {
-    username: 'password',
   },
 }
 
