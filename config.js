34133b942c18ef65364d7806073d6585a3961006
RajmondKurti112 <159893027+RajmondKurti112@users.noreply.github.com>
2024-02-16 11:19:53 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,15 @@
 const config = {
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
-  challenge: false,
+  challenge: true,
   users: {
-    // username: 'password', you can add multiple users.
-    interstellar: 'password',
+    // username: 'password', you can add multiple users
   },
 }
-export default config
+
+config.users[process.env.USER1] = process.env.USER1P;
+config.users[process.env.USER2] = process.env.USER2P;
+config.users[process.env.USER3] = process.env.USER3P;
+config.users.interstellar = 'password';
+
+export default config;
